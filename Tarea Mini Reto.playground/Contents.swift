98f374ec1573ce3f51b39a028e9e2502ae244864
LorenaGomezV <62686508+LorenaGomezV@users.noreply.github.com>
2020-04-03 23:53:36 -0600

    Add files via upload

---
+++ Tarea Mini Reto.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+for casino in 0...100 {
+    
+    if casino % 5 == 0 {
+        print ("\(casino) Bingo...")}
+        
+    else if casino >= 30 && casino <= 40 {
+        print("\(casino)\t viva Swift!!!")}
+        
+    else if casino % 2 == 0
+    {print ("\(casino)  Par ")}
+        
+    else if casino % 2 != 0
+    {print ("\(casino)  Impar")}
+
+
+}
+
